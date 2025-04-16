d88a4d6c639eb28132671a70694711b4ae4a4269
sanjay-2684Coder <sanjaydangi9294@gmail.com>
2025-04-16 18:29:15 +0000

    comited

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 434543342
+let accountName = "Sanjay"
+var accountPassword = "122344"
+accountCity = "Indore"
+accountCity = "BHOPAL"
+
+//accountId = 5656435  not allowed
+
+console.table([accountId,accountName,accountPassword,accountCity])
+
+/*
+prefer not to use var because of issue in block scoped and functional scoped
+ */
