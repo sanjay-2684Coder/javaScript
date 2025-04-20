7bb00eade6aaa607cd5364fc96f6c6780f32aaed
sanjay-2684Coder <sanjaydangi9294@gmail.com>
2025-04-20 11:00:57 +0000

    all

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,16 @@
+const bigNumber = 33454534594646n;
+//console.log(typeof bigNumber);
+
+//console.log(2 > null);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log(2 == "2");
