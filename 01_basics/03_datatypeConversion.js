3556ac1e28d8d272dd073ab2bbd1d9c9565590c2
sanjay-2684Coder <sanjaydangi9294@gmail.com>
2025-04-23 18:40:00 +0000

    done

--- 01_basics/03_datatypeConversion.js
+++ 01_basics/03_datatypeConversion.js
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "Sanjay"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+//======Number========//
+const score = 400;
+// console.log(score);
+
+// const num = new Number(400);
+// console.log(num)
+// console.log(num.toString().length);
+// console.log(num.toFixed(2));
+
+const another  = 532.45643;
+//console.log(another.toPrecision(6));
+//console.log(another.toExponential(2));
+
+const other = 1000000;
+//console.log(other.toLocaleString('en-IN'));
+
+//=============================================================//
+//=====Maths=======//
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(2.6))
+// console.log(Math.ceil(2.1));
+// console.log(Math.floor(2.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+console.log(Math.random());
+//console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
